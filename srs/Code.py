3a3adb5a67a3bbc04b7b1e9e0e4b9675f769df45
Aisha Bazylzhanova <78090663+aishabazylzhanova@users.noreply.github.com>
2021-09-19 02:03:40 +0600

    Update Code.py

--- srs/Code.py
+++ srs/Code.py
@@ -1,1 +1,8 @@
-print(0)
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+import pprint
+
+print("Please, enter a positive integer number: ")
+x = int(input())
+
+pprint.pprint(cg.get_coins_markets(vs_currency='usd', per_page=x))
